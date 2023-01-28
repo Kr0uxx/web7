fdac9a8e818bee15ba9e8dc68918322108f53e74
Kr0uxx <74461517+Kr0uxx@users.noreply.github.com>
2023-01-28 13:07:55 +0300

    Update main.py

---
+++ files/main.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import pygame
+import requests
+
+coord = input()
+cam = input("sat or map?")
+a, b = input().split()
+map_request = f"https://static-maps.yandex.ru/1.x/?ll={coord}&spn={a},{b}&l={cam}"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
